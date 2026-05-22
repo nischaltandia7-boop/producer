a57e7f8d341957e86f0930a86eb1e3ed7d1fee6f
Nischal Tandia <nischal.tandia@tripxl.com>
2026-05-22 17:57:46 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ProducerModule } from './producer/producer.module';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: 'KAFKA_SERVICE',
-
-        transport: Transport.KAFKA,
-
-        options: {
-          client: {
-            clientId: 'producer',
-            brokers: ['kafka.kafka.svc.cluster.local:9092'],
-          },
-
-          consumer: {
-            groupId: 'producer-consumer',
-          },
-        },
-      },
-    ]),
+    ProducerModule
   ],
 
   controllers: [AppController],

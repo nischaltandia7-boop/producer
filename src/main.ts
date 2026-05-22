f7791330808ba8d77215729e6715173ac013de58
Nischal Tandia <nischal.tandia@tripxl.com>
2026-05-22 17:30:32 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'service-b',
+        brokers: [process.env.KAFKA_BROKER || 'kafka.kafka.svc.cluster.local:9092'],
+      },
+
+      consumer: {
+        groupId: 'service-b-group',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  console.log(`HTTP server running on port ${process.env.PORT || 3000}`);
+  console.log('Kafka consumer is listening...');
 }
+
 bootstrap();

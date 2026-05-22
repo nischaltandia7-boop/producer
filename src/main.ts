d9ec5273b6d80d8a0769f8d4fa2db361bd0287d3
Nischal Tandia <nischal.tandia@tripxl.com>
2026-05-22 18:00:27 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.KAFKA,
     options: {

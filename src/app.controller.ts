08e2be28f74e53a7b066495fb562a0f2ec4d30dc
Nischal Tandia <nischal.tandia@tripxl.com>
2026-05-22 16:48:33 +0530

    added CI pipeline

---
+++ src/app.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Get, Inject, OnModuleInit, Post } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+
+@Controller()
+export class AppController implements OnModuleInit {
+  constructor(
+    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
+  ) { }
+
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+  }
+
+
+  @Get('send')
+  async sendMessage() {
+
+    try {
+    
+      await this.kafkaClient.emit('test', {
+
+        message: 'Hello from service-a',
+        time: new Date().toISOString(),
+        
+      });
+
+      console.log("msg sent")
+    } catch (error) {
+      console.log(error)
+    }
+
+
+    return { success: true, message: 'Message sent to Kafka' };
+  }
+
+  @Post('send')
+  async createMessage(@Body() body: any){
+    try {
+      this.kafkaClient.emit('test',body)
+      console.log("msg sent")
+    } catch (error) {
+      
+    }
+    return {success:true, message: body}
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'my-project',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'my-project-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
